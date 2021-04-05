f9d27a9e52540a92712cf6120af94f9eba1fffa8
Smoyz <SmoyziA6@gmail.com>
2021-04-05 10:50:58 +0200

    Fix d'une redirection posant problème

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return redirect('/hunt');
+});
+
 Route::get('/hunt', function () {
     return view('hunt');
 });
